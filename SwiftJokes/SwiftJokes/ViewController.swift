129961605b2e4b2c0f888129a8176fe7ec222dfe
Saamer Mansoor <i@saamer.me>
2019-09-03 12:51:32 -0400

    Single Page Blank Swift Storyboard

---
+++ SwiftJokes/SwiftJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftJokes
+//
+//  Created by Saamer Mansoor on 2019-09-03.
+//  Copyright © 2019 Saamer Mansoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
