f460130623da2cfa766347ebe88600520d630ab8
Saamer Mansoor <i@saamer.me>
2019-09-10 00:33:54 -0400

    Made the UI look pretty

--- SwiftJokes/SwiftJokes/CategoryCollectionViewCell.swift
+++ SwiftJokes/SwiftJokes/CategoryCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Saamer Mansoor. All rights reserved.
 //
 import UIKit
+
 class CategoryCollectionViewCell: UICollectionViewCell {
     
     static var identifier: String = "CategoryCollectionViewCell"
@@ -14,15 +15,32 @@ class CategoryCollectionViewCell: UICollectionViewCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         let textLabel = UILabel(frame: .zero)
-        textLabel.text = "Hi"
         textLabel.translatesAutoresizingMaskIntoConstraints = false
         self.contentView.addSubview(textLabel)
-        NSLayoutConstraint.activate([
-            self.contentView.centerXAnchor.constraint(equalTo: textLabel.centerXAnchor),
-            self.contentView.centerYAnchor.constraint(equalTo: textLabel.centerYAnchor)
-        ])
+        let horizontalConstraint2 = self.contentView.centerXAnchor.constraint(equalTo: textLabel.centerXAnchor)
+        let verticalConstraint2 = self.contentView.centerYAnchor.constraint(equalTo: textLabel.centerYAnchor)
+        NSLayoutConstraint.activate([horizontalConstraint2, verticalConstraint2])
+        let margins = self.contentView.layoutMarginsGuide
+        textLabel.leftAnchor.constraint(equalTo: margins.leftAnchor, constant: 20).isActive = true
+        textLabel.rightAnchor.constraint(equalTo: margins.rightAnchor, constant: -20).isActive = true
+        textLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        textLabel.numberOfLines = 0
+        textLabel.textColor = UIColor.label
+        
+        textLabel.textAlignment = NSTextAlignment.center
+        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 0.5
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+
         self.textLabel = textLabel
         self.reset()
     }
--- SwiftJokes/SwiftJokes/ListViewController.swift
+++ SwiftJokes/SwiftJokes/ListViewController.swift
@@ -6,25 +6,46 @@
 //  Copyright © 2019 Saamer Mansoor. All rights reserved.
 //
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    var jokeList: [String] = []
     let leftAndRightPaddings: CGFloat = 80.0
-    let numberOfItemsPerRow: CGFloat = 7.0
-    let screenSize: CGRect = UIScreen.main.bounds
+    var numberOfItemsPerRow: CGFloat = 1.0
+    var collectionView: UICollectionView!
+
+    //    let numberOfItemsPerRow: CGFloat = 1
+    var screenSize: CGRect = UIScreen.main.bounds
     private let cellReuseIdentifier = "CategoryCollectionViewCell"
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
     
-    // MARK: Base function override
+    // MARK: Base function overrides
     override func viewDidLoad()
     {
         super.viewDidLoad()
         let flowLayout = UICollectionViewFlowLayout()
-        let collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: flowLayout)
+        collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: flowLayout)
         collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: cellReuseIdentifier)
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.backgroundColor = UIColor.cyan
+        collectionView.backgroundColor = UIColor.systemTeal
         self.view.addSubview(collectionView)
+        LoadApi(collectionView: collectionView)
+    }
+    
+    func LoadApi(collectionView: UICollectionView){
+        Alamofire.request("https://api.icndb.com/jokes").responseJSON { response in
+                if let result = response.result.value {
+                    // { "type": "success", "value": [ { "id": 1, "joke": "MacGyver can build an airplane out of gum and paper clips. Chuck Norris can kill him and take it.", "categories": ["explicit"] }]}
+                    let json = JSON(result)
+                    print(json["type"]) // Prints "success"
+                    for item in json["value"].arrayValue {
+                        self.jokeList.append(item["joke"].stringValue)
+                    }
+                    collectionView.reloadData()
+            }
+        }
     }
     
     // MARK: 1st Function required in order to implement the UICollectionViewDataSource protocol
@@ -37,7 +58,11 @@ class ListViewController: UIViewController, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! CategoryCollectionViewCell
-        cell.backgroundColor = UIColor.green
+        cell.backgroundColor = UIColor.systemRed
+        if (jokeList.count > 0)
+        {
+            cell.textLabel.text = jokeList[indexPath.row]
+        }
         return cell
     }
     
